46a190632174909ef358d6835dd505b3cd075372
Parameshwari2004 <116253214+Parameshwari2004@users.noreply.github.com>
2023-12-21 10:53:47 +0530

    Add files via upload

---
+++ ThirdSum.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class ThirdSum {
+
+	public static void main(String[] args) {
+
+		int a = 2;
+		int b = 5;
+
+		int c = 2;
+		int d = 5;
+		int e = 6;
+
+		int f = 2;
+		int g = 4;
+		int h = 5;
+
+		System.out.println(a + " " + b);
+		System.out.println(c + " " + d + " " + e);
+		System.out.println(f + " " + g + " " + h);
+
+	}
+
+}
