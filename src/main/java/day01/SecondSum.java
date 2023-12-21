fd244ac045437d893b5922f30114b7158bdcd875
Parameshwari2004 <116253214+Parameshwari2004@users.noreply.github.com>
2023-12-21 10:56:47 +0530

    Add files via upload

---
+++ src/main/java/day01/SecondSum.java
@@ -0,0 +1,23 @@
+package day01;
+
+public class SecondSum {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 4;
+		
+		int c = 2;
+		int d = 4;
+		
+		int e = 2;
+		int f = 4;
+		
+		System.out.println(a+" "+b);
+		System.out.println(c+" "+d);
+		System.out.println(e+" "+f);
+		
+
+	}
+
+}
